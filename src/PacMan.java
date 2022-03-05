64e338b0787a56f66c7f3c72e3a0f307032e4fb2
LarsingDash <l.villevoye@student.avans.nl>
2022-03-05 16:46:30 +0100

    Started Repo

---
+++ src/PacMan.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PacMan extends Application {
+    public static void main(String[] args) {
+        PacMan.launch();
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
